02c73a1da33439b2a0ed87f5cb985e87ab3c140d
Mandar Bavdane <mandarbavdane@gmail.com>
2025-07-12 22:01:55 +0530

    minor changes in Art.jsx

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -1,13 +1,13 @@
-import { featureLists, goodLists } from "../../constants";
+import gsap from "gsap";
 import { useMediaQuery } from "react-responsive";
 import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
+import { featureLists, goodLists } from "../../constants/index.js";
 
 const Art = () => {
-  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isMobile = useMediaQuery({ maxWidth: 767 });
 
   useGSAP(() => {
-    const start = isMobile ? "top 20" : "top top";
+    const start = isMobile ? "top 20%" : "top top";
 
     const maskTimeline = gsap.timeline({
       scrollTrigger: {
@@ -15,22 +15,18 @@ const Art = () => {
         start,
         end: "bottom center",
         scrub: 1.5,
-        pint: true,
+        pin: true,
       },
     });
 
     maskTimeline
-      .to("will-fade", {
-        opacity: 0,
-        stagger: 0.2,
-        ease: "power1.inOut",
-      })
-      .to("masked-img", {
+      .to(".will-fade", { opacity: 0, stagger: 0.2, ease: "power1.inOut" })
+      .to(".masked-img", {
         scale: 1.3,
         maskPosition: "center",
         maskSize: "400%",
         duration: 1,
-        ease: "power1.inOut",
+        ease: "power1.inOut ",
       })
       .to("#masked-content", { opacity: 1, duration: 1, ease: "power1.inOut" });
   });
@@ -60,10 +56,7 @@ const Art = () => {
 
           <ul className="space-y-4 will-fade">
             {featureLists.map((feature, index) => (
-              <li
-                key={index}
-                className="flex items-center justify-center gap-2"
-              >
+              <li key={index} className="flex items-center justify-start gap-2">
                 <img src="/images/check.png" alt="check" />
                 <p className="md:w-fit w-60">{feature}</p>
               </li>
@@ -73,7 +66,7 @@ const Art = () => {
 
         <div className="masked-container">
           <h2 className="will-fade">Sip-Worthy Perfection</h2>
-          <div id="maskeded-content">
+          <div id="masked-content">
             <h3>Made with Craft, Poured with Passion</h3>
             <p>
               This isn't just a drink. It's a carefully crafted moment made just
